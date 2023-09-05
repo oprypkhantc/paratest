75ad284a53556200d786e35a35dba756745354eb
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2023-09-05 22:04:29 +0300

    Update phpunit-wrapper.php

--- bin/phpunit-wrapper.php
+++ bin/phpunit-wrapper.php
@@ -6,6 +6,9 @@ use ParaTest\WrapperRunner\ApplicationForWrapperWorker;
 use ParaTest\WrapperRunner\WrapperWorker;
 
 (static function (): void {
+    error_reporting(-1);
+	ini_set('display_errors', 'On');
+    
     $getopt = getopt('', [
         'status-file:',
         'progress-file:',
